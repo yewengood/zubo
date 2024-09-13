f9b0dcd93716e5f7dc912e84bdd5cc94cff8d5a7
yewengood <152235769+yewengood@users.noreply.github.com>
2024-09-13 11:48:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -141,9 +141,9 @@ def updateChannelUrlsM3U(channels, template_channels):
                             total_urls = len(filtered_urls)
                             for index, url in enumerate(filtered_urls, start=1):
                                 if is_ipv6(url):
-                                    url_suffix = f"$LR•IPV6" if total_urls == 1 else f" "
+                                    url_suffix = f" " if total_urls == 1 else f" "
                                 else:
-                                    url_suffix = f"$LR•IPV4" if total_urls == 1 else f" "
+                                    url_suffix = f" " if total_urls == 1 else f" "
                                 if '$' in url:
                                     base_url = url.split('$', 1)[0]
                                 else:
