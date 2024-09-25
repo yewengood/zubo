7e2334ba8aec9c6a5698b0500376e78a30fdb415
yewengood <152235769+yewengood@users.noreply.github.com>
2024-09-25 17:45:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ source_urls = [
     "https://raw.githubusercontent.com/yewengood/gdzb/main/speedtest/zubo_fofa.txt",
     "https://raw.githubusercontent.com/yewengood/-/main/888.txt",
     "http://我不是.摸鱼儿.top/live.php",
-    "https://judy.567852.xyz/live/playlist.m3u?token=549293861986921",
+    "https://raw.githubusercontent.com/yewengood/ktv/refs/heads/main/speedtest/gdtv_fofa.txt",
     "http://free.xiptv.x10.bz/p3p.php"
     #"https://jihulab.com/ss3234741/sj/raw/2a8f0afa421a98836bc891698446c7271c2f32f3/lib/live2.txt",
     #"https://gitcode.net/ygbh66/666/raw/master/x.txt",
